f93dbf1c5d7be94de7cb07085988cbbe3ce015c8
JunB <51225580+bgj0127@users.noreply.github.com>
2024-09-02 12:06:38 +0900

    [Fix] SSL 제거

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
 const express = require("express");
-const https = require("https");
-const fs = require("fs");
 
 const app = express();
 const cors = require("cors");
 const bp = require("body-parser");
 const userRouter = require("./routes/userRouter");
-
-const options = {
-  key: fs.readFileSync("./mkcert/localhost+2-key.pem"),
-  cert: fs.readFileSync("./mkcert/localhost+2.pem"),
-};
 
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
@@ -22,9 +15,4 @@ app.get("/", (req, res) => {
   // res.send("^^b");
 });
 
-const httpsServer = https.createServer(options, app);
-
-httpsServer.listen(3000, () => {
-  console.log(options);
-  console.log("HTTPS Server running on port 3000");
-});
+app.listen(3000);
